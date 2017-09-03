7780290842298467eb9c03958f23ba31f410f7dc
phmima <michael.matela@e-horizon.com>
2017-09-04 00:53:25 +0800

    Profile model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903164127) do
+ActiveRecord::Schema.define(version: 20170903164937) do
 
-  create_table "samples", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.string   "sample"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "profiles", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "username"
     t.string   "email"
     t.string   "encrypted_password"
