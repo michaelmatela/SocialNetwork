85e8c7b371573d1e41d12cd65e676503c0b7c54f
phmima <michael.matela@e-horizon.com>
2017-09-04 12:30:36 +0800

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903164937) do
+ActiveRecord::Schema.define(version: 20170904042954) do
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "email"
+    t.string   "encrypted_password"
+    t.string   "salt"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
 
 end
